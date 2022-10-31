4d392ac70027942e4d7678be3e3785ac67d18723
NotRealSean <89067287+NotRealSean@users.noreply.github.com>
2022-10-31 20:18:57 +0700

    Update framework, add update penal, change exit button and design about save-load system

--- Program.cs
+++ Program.cs
@@ -16,19 +16,18 @@ class coreGame
 {
     public static void chapter1()
     {
-        //textTool.textGen("enter smt",1 /*text speed*/, true /*clear console*/, true /*add press continue*/);
+        //textTool.textGen("enter smt",1 /*text speed*/, true /*clear console*/, true /*add press continue*/, 1 /*delay after text complete*/);
+        textTool.textGen("<Chapter I - Beginning Adventure>", 100, true, true, 3000);
         textTool.textGen("Hi this is my first story test text genetator.", 1, true, true);
         textTool.textGen("Test story text generator 2", 1, true, true);
 
-        Console.Clear();
-        textTool.textGen("This is very ");
-        Thread.Sleep(1000);
-        textTool.textGen("very ");
-        Thread.Sleep(1000);
+        textTool.textGen("This is very ", 1, true, false, 1000);
+        textTool.textGen("very ", 1, false, false, 1000);
         textTool.textGen("cool text generation", 1, false, true);
 
 
-        textTool.textGen("The End", 1, true, true);
+        textTool.textGen("The End", 1, true, false, 1000);
+        textTool.textGen("\n<Chapter I - Normal Ending>", 100, false, true, 3000);
     }
     public static void chapter2()
     {
@@ -44,7 +43,7 @@ class Menu
             Console.Clear();
             //Main menu
             Console.WriteLine("=======================================================");
-            Console.WriteLine("\t\tA Text-base game name(Demo)\n\t1 Play\n\t2 Load(Not work yet)\n\t3 Guide\n\t4 Credits\n\t5 Exit\n\n");
+            Console.WriteLine("\t\tA Text-base game name(Demo)\n\t1 Play\n\t2 Load(Not work yet)\n\t3 Guide\n\t4 Credits\n\t5 Update\n\n\t9 Exit");
             Console.WriteLine("=======================================================");
             textTool.textGen("Key command -=>");
             string inPut = Console.ReadLine();
@@ -105,6 +104,14 @@ class Menu
             }
             else if (inPut == "5")
             {
+                Console.Clear();
+                Console.WriteLine("\t\tUpdate");
+                Console.WriteLine("- Add update option to keep things up-to-date\n- Update framework(add delay after text generate)\n- Change exit key to 9\n");
+                textTool.textGen("Press anykey to go back to main menu...");
+                Console.ReadKey();
+            }
+            else if (inPut == "9")
+            {
                 break;
             }
             else if (inPut == "" || inPut == " ")
@@ -122,6 +129,37 @@ class Menu
 }
 class textTool
 {
+    public static void textGen(string text, int textSpeed, bool clearConsole, bool pressOnKey, int delay)
+    {
+        if (clearConsole == true)
+        {
+            Console.Clear();
+        }
+        if (pressOnKey == true)
+        {
+            for (int i = 0; i < text.Length + 1; i++)
+            {
+                if (i >= text.Length)
+                {
+                    Thread.Sleep(delay);
+                    Console.WriteLine("\n\n-\tPress space bar or enter to continue\t-");
+                    break;
+                }
+                Console.Write(text[i]);
+                Thread.Sleep(textSpeed);
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter && Console.ReadKey(true).Key != ConsoleKey.Spacebar){}
+        }
+        if (pressOnKey == false)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+                Thread.Sleep(textSpeed);
+            }
+            Thread.Sleep(delay);
+        }
+    }
     public static void textGen(string text, int textSpeed, bool clearConsole, bool pressOnKey)
     {
         if (clearConsole == true)
@@ -180,4 +218,8 @@ class textTool
             Thread.Sleep(1);
         }
     }
+}
+class save
+{
+    //some save code
 }
