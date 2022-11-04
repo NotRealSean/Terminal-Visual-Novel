860936de9b6ea28d68cee568b09f223861c62021
NotRealSean <89067287+NotRealSean@users.noreply.github.com>
2022-11-04 08:08:15 +0700

    Revert "Add saveload function and test json file"
    This reverts commit f7def3426547c944f8cb1bd094b8b5fb6bc5c454.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq.Expressions;
-using System.Xml.Linq;
-using System;
+﻿using System;
 using System.Net.NetworkInformation;
 using System.Dynamic;
 using System.Globalization;
@@ -12,32 +10,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.Json.Nodes;
 
-public class coreGame
+class coreGame
 {
     public static void chapter1()
     {
         //textTool.textGen("enter smt",1 /*text speed*/, true /*clear console*/, true /*add press continue*/, 1 /*delay after text complete*/);
-        TextTool.TextGen("<Chapter I - Beginning Adventure>", 100, true, true, 3000);
-        TextTool.TextGen("Hi this is my first story test text genetator.", 1, true, true);
-        TextTool.TextGen("Test story text generator 2", 1, true, true);
+        textTool.textGen("<Chapter I - Beginning Adventure>", 100, true, true, 3000);
+        textTool.textGen("Hi this is my first story test text genetator.", 1, true, true);
+        textTool.textGen("Test story text generator 2", 1, true, true);
 
-        TextTool.TextGen("This is very ", 1, true, false, 1000);
-        TextTool.TextGen("very ", 1, false, false, 1000);
-        TextTool.TextGen("cool text generation", 1, false, true);
+        textTool.textGen("This is very ", 1, true, false, 1000);
+        textTool.textGen("very ", 1, false, false, 1000);
+        textTool.textGen("cool text generation", 1, false, true);
 
 
-        TextTool.TextGen("The End", 1, true, false, 1000);
-        TextTool.TextGen("\n<Chapter I - Normal Ending>", 100, false, true, 3000);
-        Saveload.save("1","1");
+        textTool.textGen("The End", 1, true, false, 1000);
+        textTool.textGen("\n<Chapter I - Normal Ending>", 100, false, true, 3000);
     }
     public static void chapter2()
     {
-        TextTool.TextGen("This is chapter 2 test text",1 ,true ,true);
-        Saveload.save("2","1");
+        textTool.textGen("This is chapter 2 test text",1 ,true ,true);
     }
 }
 class Menu
@@ -51,7 +44,7 @@ class Menu
             Console.WriteLine("=======================================================");
             Console.WriteLine("\t\tA Text-base game name(Demo)\n\t1 Play\n\t2 Load(Not work yet)\n\t3 Guide\n\t4 Credits\n\t5 Update\n\n\t9 Exit");
             Console.WriteLine("=======================================================");
-            TextTool.TextGen("Key command -=>");
+            textTool.textGen("Key command -=>");
             string inPut = Console.ReadLine();
             if (inPut == "1")
             {
@@ -61,7 +54,7 @@ class Menu
                     Console.WriteLine("=======================================================");
                     Console.WriteLine("\t\tSelect Chapter\n\tChapter 1\n\tChapter 2\n\n\t9 Back to main menu teest");
                     Console.WriteLine("=======================================================");
-                    TextTool.TextGen("Key command -=>");
+                    textTool.textGen("Key command -=>");
                     string chapterSelect = Console.ReadLine();
                     if (chapterSelect == "1")
                     {
@@ -87,30 +80,26 @@ class Menu
                 while (true)
                 {
                     Console.Clear();
-                    
-                        JsonNode _nodedata = Saveload.read();
-                        string _stringSaveList = _nodedata[0]["save"].ToString();
-                        int _intSaveList = Convert.ToInt32(_stringSaveList);
-                        for (int i = 0; i < _intSaveList; i++)
-                        {
-                            Console.WriteLine("Chapter " + i+1);
-                        }
-                        TextTool.TextGen("Key command -=>");
-                        string loadChapter = Console.ReadLine();
-                        if (loadChapter == "9")
-                        {
-                            break;
-                        }
-                        int _intLoadChapter = Convert.ToInt32(loadChapter);
-                        Saveload.load(_intLoadChapter, _intSaveList);
-
+                    JsonNode _nodedata = Saveload.read();
+                    string _stringSaveList = _nodedata[0]["save"].ToString();
+                    Console.WriteLine("Your last save is " + _stringSaveList + "(9 Exit)");
+                    TextTool.TextGen("Key command -=>");
+                    string loadChapter = Console.ReadLine();
+                    if (loadChapter == "9")
+                    {
+                        break;
+                    }
+                    try
+                    {
+                        int loadChapterInt = Convert.ToInt32(loadChapter);
+                        string _stringChapter = _nodedata[loadChapterInt-1]["save"].ToString();
+                        Saveload.load(_stringChapter);
+                    }
+                    catch (Exception)
+                    {
                         TextTool.TextGen("You enter worng key or chapter doesn\'t exist");
                         Console.ReadKey();
-                    
-
-                        TextTool.TextGen("something went worng");
-                        Console.ReadKey();
-                        break;
+                    }
                 }
             }
             else if (inPut == "3")
@@ -122,14 +111,14 @@ class Menu
                 Console.WriteLine("\t\tHow to play this game\n");
                 Console.WriteLine("Just press \"space bar or enter\" to continue reading\n");
 
-                TextTool.TextGen("Press anykey to go back to main menu...");
+                textTool.textGen("Press anykey to go back to main menu...");
                 Console.ReadKey();
             }
             else if (inPut == "4")
             {
                 Console.Clear();
                 Console.WriteLine("\t\tHead project\nNotRealSean\n\n\t\tStory writer\nClearX2\n");
-                TextTool.TextGen("Press anykey to go back to main menu...");
+                textTool.textGen("Press anykey to go back to main menu...");
                 Console.ReadKey();
             }
             else if (inPut == "5")
@@ -137,7 +126,7 @@ class Menu
                 Console.Clear();
                 string update = File.ReadAllText("_update.txt");
                 Console.WriteLine(update);
-                TextTool.TextGen("Press anykey to go back to main menu...");
+                textTool.textGen("Press anykey to go back to main menu...");
                 Console.ReadKey();
             }
             else if (inPut == "9")
@@ -157,9 +146,9 @@ class Menu
         }
     }
 }
-class TextTool
+class textTool
 {
-    public static void TextGen(string text, int textSpeed, bool clearConsole, bool pressOnKey, int delay)
+    public static void textGen(string text, int textSpeed, bool clearConsole, bool pressOnKey, int delay)
     {
         if (clearConsole == true)
         {
@@ -190,7 +179,7 @@ class TextTool
             Thread.Sleep(delay);
         }
     }
-    public static void TextGen(string text, int textSpeed, bool clearConsole, bool pressOnKey)
+    public static void textGen(string text, int textSpeed, bool clearConsole, bool pressOnKey)
     {
         if (clearConsole == true)
         {
@@ -220,7 +209,7 @@ class TextTool
             }
         }
     }
-    public static void TextGen(string text, int textSpeed, bool clearConsole)
+    public static void textGen(string text, int textSpeed, bool clearConsole)
     {
         if (clearConsole == true)
         {
@@ -232,7 +221,7 @@ class TextTool
             Thread.Sleep(textSpeed);
         }
     }
-    public static void TextGen(string text, int textSpeed)
+    public static void textGen(string text, int textSpeed)
     {
         for (int i = 0; i < text.Length; i++)
         {
@@ -240,7 +229,7 @@ class TextTool
             Thread.Sleep(textSpeed);
         }
     }
-    public static void TextGen(string text)
+    public static void textGen(string text)
     {
         for (int i = 0; i < text.Length; i++)
         {
@@ -249,54 +238,33 @@ class TextTool
         }
     }
 }
-public class Saveload : coreGame
+class saveTool
 {
-    public static void save(string chapter, string route)
+    public static void save(string chapter)
     {
-        string _filename = "save.json";
-        object[] arr = new object[1];
-        var _save = new dataSave
-        {
-            save = chapter,
-            route = route
-        };
-
-        arr[0] = _save;
-
-        string _savedata = JsonSerializer.Serialize(arr);
-        File.WriteAllText(_filename, _savedata);
+        
     }
     public static void load(int chapter, int maxList)
     {
-        if (chapter == 1 && chapter <= maxList)
+        if (chapter == "1")
         {
             chapter1();
         }
-        else if (chapter == 2 && chapter <= maxList)
+        else if (chapter == "2")
         {
             chapter2();
         }
         else
         {
-            TextTool.TextGen("Chapter fail to load", 1, true, false, 1000);
+            Console.WriteLine("Chapter not found");
         }
     }
     public static void delete(string chapter)
     {
-        string _filename = "save.json";
-        string del = "";
-        File.WriteAllText(_filename,del);
-    }
-    public static JsonNode read()
-    {
-        string _filename = "save.json";
-        string jsondata = File.ReadAllText(_filename);
-        var _loadJson = JsonNode.Parse(jsondata);
-        return _loadJson;
+
     }
 }
 public class dataSave
 {
     public string save {get; set;}
-    public string route {get; set;}
 }
