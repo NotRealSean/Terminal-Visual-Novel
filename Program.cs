5fa1e24b95377cf1c24a22e30044d193c9f349c7
NotRealSean <89067287+NotRealSean@users.noreply.github.com>
2022-11-06 20:15:53 +0700

    Change TextTool syntax a little bit and re-write guild, credits message

--- Program.cs
+++ Program.cs
@@ -18,13 +18,13 @@ public class coreGame
 {
     public static void chapter1()
     {
-        //textTool.textGen("enter smt",1 /*text speed*/, true /*clear console*/, true /*add press continue*/, 1 /*delay after text complete*/);
-        textTool.textGen("This is chapter 1 test text", 1, true, true);
+        //TextTool.textGen("enter smt",1 /*text speed*/, true /*clear console*/, true /*add press continue*/, 1 /*delay after text complete*/);
+        TextTool.TextGen("This is chapter 1 test text", 1, true, true);
         Saveload.save("1","1");
     }
     public static void chapter2()
     {
-        textTool.textGen("This is chapter 2 test text", 1, true, true);
+        TextTool.TextGen("This is chapter 2 test text", 1, true, true);
         Saveload.save("2","1");
     }
 }
@@ -39,7 +39,7 @@ class Menu
             Console.WriteLine("=======================================================");
             Console.WriteLine("\t\tA Text-base game name(Demo)\n\t1 Play\n\t2 Quick Load\n\t3 Guide\n\t4 Credits\n\t5 Update\n\n\t9 Exit");
             Console.WriteLine("=======================================================");
-            textTool.textGen("Key command -=>");
+            TextTool.TextGen("Key command -=>");
             string inPut = Console.ReadLine();
             if (inPut == "1")
             {
@@ -50,7 +50,7 @@ class Menu
                     Console.WriteLine("=======================================================");
                     Console.WriteLine("\t\tSelect Chapter\n\tChapter 1\n\tChapter 2\n\n\t9 Back to main menu teest");
                     Console.WriteLine("=======================================================");
-                    textTool.textGen("Key command -=>");
+                    TextTool.TextGen("Key command -=>");
                     string chapterSelect = Console.ReadLine();
                     if (chapterSelect == "1")
                     {
@@ -86,20 +86,17 @@ class Menu
             {
                 //Guide
                 Console.Clear();
-                Console.WriteLine("\t\tHello and Welcome\n");
-                Console.WriteLine("Welcome to my first text-base visual novel game! this game is NOT full game just yet(And it maybe demo forever)\n\n");
-                Console.WriteLine("\t\tHow to play this game\n");
-                Console.WriteLine("Just press \"space bar or enter\" to continue reading\n");
+                TextTool.TextGen("\t\tNew better guide is coming soon\n");
 
-                textTool.textGen("Press anykey to go back to main menu...");
+                TextTool.TextGen("Press anykey to go back to main menu...");
                 Console.ReadKey();
             }
             else if (inPut == "4")
             {
                 //Credits
                 Console.Clear();
-                Console.WriteLine("\t\tHead project\nNotRealSean\n\n\t\tStory writer\nClearX2\n");
-                textTool.textGen("Press anykey to go back to main menu...");
+                TextTool.TextGen("New better credits is coming soon");
+                TextTool.TextGen("Press anykey to go back to main menu...");
                 Console.ReadKey();
             }
             else if (inPut == "5")
@@ -107,7 +104,7 @@ class Menu
                 Console.Clear();
                 string update = File.ReadAllText("_update.txt");
                 Console.WriteLine(update);
-                textTool.textGen("Press anykey to go back to main menu...");
+                TextTool.TextGen("Press anykey to go back to main menu...");
                 Console.ReadKey();
             }
             else if (inPut == "9")
@@ -127,9 +124,27 @@ class Menu
         }
     }
 }
-class textTool
+/*
+
+
+
+
+
+
+*    WARNING THIS IS TOOLS FUNCTION ZONE!
+
+*   IF YOU EDIT ANYTHING IN THIS YOUR FUNCTION MIGHT BROKE OR CANT BE USE!
+*   BECAREFUL IF YOU WANT TO EDIT SOMETHING.
+
+
+
+
+
+
+*/
+class TextTool
 {
-    public static void textGen(string text, int textSpeed, bool clearConsole, bool pressOnKey, int delay)
+    public static void TextGen(string text, int textSpeed, bool clearConsole, bool pressOnKey, int delay)
     {
         if (clearConsole == true)
         {
@@ -160,7 +175,7 @@ class textTool
             Thread.Sleep(delay);
         }
     }
-    public static void textGen(string text, int textSpeed, bool clearConsole, bool pressOnKey)
+    public static void TextGen(string text, int textSpeed, bool clearConsole, bool pressOnKey)
     {
         if (clearConsole == true)
         {
@@ -190,7 +205,7 @@ class textTool
             }
         }
     }
-    public static void textGen(string text, int textSpeed, bool clearConsole)
+    public static void TextGen(string text, int textSpeed, bool clearConsole)
     {
         if (clearConsole == true)
         {
@@ -202,7 +217,7 @@ class textTool
             Thread.Sleep(textSpeed);
         }
     }
-    public static void textGen(string text, int textSpeed)
+    public static void TextGen(string text, int textSpeed)
     {
         for (int i = 0; i < text.Length; i++)
         {
@@ -210,7 +225,7 @@ class textTool
             Thread.Sleep(textSpeed);
         }
     }
-    public static void textGen(string text)
+    public static void TextGen(string text)
     {
         for (int i = 0; i < text.Length; i++)
         {
--- README.txt
+++ README.txt
@@ -1,6 +1,11 @@
 This is a small project about make game with only console control
 
-This project name is "ConsoleTextBaseGame" just a small project by a boring person name NotRealSean
-This game may not be full game release because I'm lazy(That's it)
+This project made by NotRealSean
 
-Nothing to say much about just have fun and you can take my idea to improve or whatever I don't mind about it
+Control:
+Menu
+You'll see "Key command -=>" at the bottom
+Type something and hit "Enter" on your keyboard
+
+Story
+just hit "Enter" or "Space bar" to continue reading
